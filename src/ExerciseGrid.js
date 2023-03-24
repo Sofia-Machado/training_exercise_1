f314b2500e15b166996557b4cec4dd66d1aa160b
Sofia Machado <sofia.machado@riva-office.pt>
2023-03-24 17:21:30 +0000

    center grid

--- src/ExerciseGrid.js
+++ src/ExerciseGrid.js
@@ -3,6 +3,7 @@ import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import { Container, Typography } from '@mui/material';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -14,34 +15,61 @@ const Item = styled(Paper)(({ theme }) => ({
 
 function FormRow() {
   return (
-    <React.Fragment>
-      <Grid item xs={4}>
-        <Item>Item</Item>
+    <>
+      <Grid item lg={1}>
+        <Item>1</Item>
       </Grid>
-      <Grid item xs={4}>
-        <Item>Item</Item>
+      <Grid item lg={1}>
+        <Item>2</Item>
       </Grid>
-      <Grid item xs={4}>
-        <Item>Item</Item>
+      <Grid item lg={1}>
+        <Item>3</Item>
       </Grid>
-    </React.Fragment>
+      <Grid item lg={1}>
+        <Item>4</Item>
+      </Grid>
+      <Grid item lg={1}>
+        <Item>5</Item>
+      </Grid>
+      <Grid item lg={1}>
+        <Item>6</Item>
+      </Grid>
+      <Grid item lg={1}>
+        <Item>7</Item>
+      </Grid>
+      <Grid item lg={1}>
+        <Item>8</Item>
+      </Grid>
+      <Grid item lg={1}>
+        <Item>9</Item>
+      </Grid>
+    </>
   );
 }
 
 export default function ExerciseGrid() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={1}>
-        <Grid container item spacing={3}>
-          <FormRow />
+    <Container maxWidth="lg">
+      <Typography align='center' variant='h3' component='h1'>Select Rows</Typography>
+      <Box sx={{ flexGrow: 1 }} mt={2} wrap>
+        <Grid container justifyContent="center" spacing={2}>
+          <Grid container sx={{ display:'flex', justifyContent:"center", alignItems:"center" }} item spacing={1}>
+            <FormRow />
+          </Grid>
+          <Grid container sx={{ display:'flex', justifyContent:"center", alignItems:"center" }} item spacing={1}>
+            <FormRow />
+          </Grid>
+          <Grid container sx={{ display:'flex', justifyContent:"center", alignItems:"center" }} item spacing={1}>
+            <FormRow />
+          </Grid>
+          <Grid container sx={{ display:'flex', justifyContent:"center", alignItems:"center" }} item spacing={1}>
+            <FormRow />
+          </Grid>
+          <Grid container sx={{ display:'flex', justifyContent:"center", alignItems:"center" }} item spacing={1}>
+            <FormRow />
+          </Grid>
         </Grid>
-        <Grid container item spacing={3}>
-          <FormRow />
-        </Grid>
-        <Grid container item spacing={3}>
-          <FormRow />
-        </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </Container>
   );
 }
